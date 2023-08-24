54175be7ae3a6da0d56e926c848c08b748fb2a94
Felipe Minozzo <edu.minozzo1995@gmail.com>
2023-08-23 21:24:52 -0300

    Aula 1

---
+++ src/pages/Favoritos/index.js
@@ -0,0 +1,35 @@
+import Banner from "components/Banner";
+import Card from "components/Card";
+import Titulo from "components/Titulo";
+import styles from './favoritos.module.css';
+import React, { useEffect, useState } from "react";
+
+function Favoritos(){
+    const [videos, setVideos] = useState([]);
+
+    useEffect(() => {
+        fetch('https://my-json-server.typicode.com/FelpsMinozzo/fake_api')
+            .then(resposta => resposta.json())
+            .then(dados => {
+                setVideos(dados)
+            })
+    }, [])
+
+    return(
+        <>
+            <Banner imagem='favoritos'/>
+            <Titulo>
+                <h1>Meus Favoritos</h1>
+            </Titulo>
+
+            <section className={styles.container}>
+                {videos.map((video) => {
+                    return <Card {...video} key={video.id}/>
+                })}
+            </section>
+        </>
+    )
+
+}
+
+export default Favoritos;
